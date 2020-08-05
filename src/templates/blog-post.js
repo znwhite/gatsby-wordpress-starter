cd8b3972e72140cd5a1b88de56c22acc3b130c61
znwhite <zwhite@webstacks.com>
2020-08-05 16:41:34 -0700

    adds images

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,13 +1,24 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function BlogPost(props){
     const { title, excerpt, content } = props.pageContext;
     return(
-        <div>
+        <Article>
             <p>{title}</p>
-            <div>{content}</div>
-        </div>
+            <div dangerouslySetInnerHTML={{__html: content }}></div>
+        </Article>
     )
 }
 
 export default BlogPost;
+
+const Article = styled.div`
+    width: 800px;
+    display: block;
+    margin: 0 auto;
+    img {
+        display: block;
+        margin: 0 auto;
+    }
+`
